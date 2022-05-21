562a69bc5c261af90c8d82e70fc584d98ba3666a
adrianongit <adriansantosrollim@gmail.com>
2022-05-21 12:55:27 -0300

    first commit

--- index.php
+++ index.php
@@ -9,8 +9,20 @@
 </head>
 <body>
 
+	<div id="root"></div>
+
 	<h3 align="center">Hello, PHP!</h3>
 	<!-- JavaScript -->
 	<script type="text/javascript" src="../src/assets/js/script.js"></script>
+	<script type="text/javascript" src="../src/assets/js/banco.js"></script>
 </body>
 </html>
+
+<?php
+
+$str = "Olá, este é apenas um texto!";
+$pattern = "/este/i";
+
+echo preg_match_all($pattern, $str);
+
+?>
---
+++ src/assets/js/banco.js
@@ -0,0 +1,20 @@
+let root = document.getElementById('root');
+
+const db = [
+	{name: 'Near', user: 'near@deathnotr'},
+	{name: 'Subaru Natsuki', user: 'sub@rezero'},
+	{name: 'Nagisa', user: 'nagisa@korusensei'},
+	{name: 'Nami', user: 'nami@onepiece'},
+	{name: 'Mikasa Alkerman', user: 'mikasa@aot'},
+	{name: 'Rampo', user: 'rampo@bsd'},
+	{name: 'Levi Alkerman', user: 'levi@aot'},
+	{name: 'Shikamaru Nara', user: 'shikamaru@naruto'},
+	{name: 'Aska Langli Soryou', user: 'aska@nge'},
+	{name: 'Shiji', user: 'shinji@nge'},
+	{name: 'Roronoa Zoro', user: 'zoro@onepiece'},
+	{name: 'Zero Two', user: 'zerotwo@ditf'},
+	{name: 'Saitama', user: 'saitama@opm'},
+	{name: 'Son Goku', user: 'songoku@db'}
+]
+
+console.table(db[0])
